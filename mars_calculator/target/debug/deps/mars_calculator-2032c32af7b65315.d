0e6c5f8a370e727a07c573b0c9a3041e240c26dd
Nick M <60850929+wariomale@users.noreply.github.com>
2022-01-24 10:10:41 +0000

    calculator

---
+++ mars_calculator/target/debug/deps/mars_calculator-2032c32af7b65315.d
@@ -0,0 +1,5 @@
+/home/runner/rust/mars_calculator/target/debug/deps/mars_calculator-2032c32af7b65315: src/main.rs
+
+/home/runner/rust/mars_calculator/target/debug/deps/mars_calculator-2032c32af7b65315.d: src/main.rs
+
+src/main.rs:
---
+++ mars_calculator/target/debug/mars_calculator.d
@@ -0,0 +1,1 @@
+/home/runner/rust/mars_calculator/target/debug/mars_calculator: /home/runner/rust/mars_calculator/src/main.rs
