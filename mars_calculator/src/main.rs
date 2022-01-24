8c103ae4dfc1a7c9d09b9a67216f2d3c45e7e1a0
sudosnugg <>
2022-01-24 12:51:36 +0000

    continued work in udemy course

--- mars_calculator/src/main.rs
+++ mars_calculator/src/main.rs
@@ -1,5 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Weight on mars: {}kg", calculate_weight_on_mars(100.0));
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input);
+    println!("Input: {}", input);
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
